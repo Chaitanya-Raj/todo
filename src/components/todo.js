4c347f32d566c7587344224b9fd3f4d3e70c1590
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-07-10 12:05:09 +0530

    add styling to list items

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = ({ todo, deleteTodo }) => {
+  const [completed, setCompleted] = useState(false);
+
+  const content = () => {
+    return completed ? (
+      <p className="content completed">
+        <strike>{todo.content}</strike>
+      </p>
+    ) : (
+      <p className="content">{todo.content}</p>
+    );
+  };
+
   return (
     <li key={todo.id}>
-      {todo.content} <button onClick={() => deleteTodo(todo.id)}>X</button>
+      <button onClick={() => setCompleted(!completed)}>
+        <span role="img" aria-label="mark-complete" id="mark-complete">
+          &#9989;
+        </span>
+      </button>
+      {content()}
+      <button onClick={() => deleteTodo(todo.id)}>
+        <span role="img" aria-label="mark-delete" id="mark-delete">
+          &#10060;
+        </span>
+      </button>
     </li>
   );
 };
