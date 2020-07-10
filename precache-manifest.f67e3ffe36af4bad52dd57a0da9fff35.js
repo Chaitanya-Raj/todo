208a4458041234f75848933dff0856ce65d1e9ab
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-07-10 13:25:15 +0530

    Updates

---
+++ precache-manifest.f67e3ffe36af4bad52dd57a0da9fff35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "687810c0b19b32182b06448391225166",
+    "url": "/Chaitanya-Raj/todo/index.html"
+  },
+  {
+    "revision": "e29475ea96ac729481e2",
+    "url": "/Chaitanya-Raj/todo/static/css/main.f865ad5d.chunk.css"
+  },
+  {
+    "revision": "59b7974022d7e12d41a0",
+    "url": "/Chaitanya-Raj/todo/static/js/2.5bd1538c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chaitanya-Raj/todo/static/js/2.5bd1538c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29475ea96ac729481e2",
+    "url": "/Chaitanya-Raj/todo/static/js/main.eda8b76b.chunk.js"
+  },
+  {
+    "revision": "39b6d05f9d9c34bde28d",
+    "url": "/Chaitanya-Raj/todo/static/js/runtime-main.fd376518.js"
+  }
+]);
